9369448b5a34282b385b5b1daf14f86a7a689c6c
Eric Zell <EricJZell@gmail.com>
2016-01-28 22:36:26 -0500

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#admin?" do
+      it "returns true when user is admin" do
+        user = FactoryGirl.create(:admin)
+        expect(user.admin?).to be true
+      end
+      it "returns false when user is not an admin" do
+        user = FactoryGirl.create(:user)
+        expect(user.admin?).to be false
+      end
+  end
 end
